42a273bf172387bfb72f3921d5718ac52b41afcb
bielvrs <142364503+bielvrs@users.noreply.github.com>
2023-11-13 23:41:12 +0000

    laço para até repetição (for)

---
+++ atividade003.js
@@ -0,0 +1,10 @@
+// Laço Para Até
+// se colocoar var invés de let eu consigo encontrar essa váriavel dentro e fora da chave
+
+for(let i=1; i<=10 ; i++){
+    console.log(i)
+}
+
+for(let i = 0; i<= 5; i++){
+    console.log("Botafogo Campeão " +i)
+}
