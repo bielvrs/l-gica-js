87b31f8ba8d129d7afd6b0149b647f4d43f5fee5
bielvrs <142364503+bielvrs@users.noreply.github.com>
2023-11-14 23:22:34 +0000

    exercícios

---
+++ exercício003.js
@@ -0,0 +1,8 @@
+//aça um programa que receba dois números inteiros e gere os números inteiros que estão no intervalo compreendido por eles. F
+let inicio = 20 
+let fim = 20
+
+while(inicio < fim-1){
+    inicio++
+    console.log(inicio)
+}
