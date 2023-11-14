d4f891a287a8fedc7cf5bdc7d02465027803fc51
bielvrs <142364503+bielvrs@users.noreply.github.com>
2023-11-14 01:17:47 +0000

    exercício de rept

---
+++ exercício001.js
@@ -0,0 +1,15 @@
+// 1.Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para que ele mostre os números um ao lado do outro.
+
+let i = 1
+while(i <= 20){
+    console.log(i)
+    i++
+}
+
+let i = 1
+let numeros
+while (i<= 20){
+    numeros = numeros + "" +i
+    i++
+}
+console.log(numeros)
